7cb8452eff9300cd7fb73255aaeb7df8844abd69
snigdhagogineni10 <snigdhagogineni10@gmail.com>
2026-08-29 13:32:10 +0530

    Update environment variable handling in config.py
    Replaced direct access to environment variables with get method for TELEGRAM_BOT_TOKEN and added GREEN_API_INSTANCE_ID and GREEN_API_TOKEN.

--- config.py
+++ config.py
@@ -8,8 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN: str = os.environ["TELEGRAM_BOT_TOKEN"]
-TWILIO_ACCOUNT_SID: str = os.environ["TWILIO_ACCOUNT_SID"]
-TWILIO_AUTH_TOKEN: str = os.environ["TWILIO_AUTH_TOKEN"]
-TWILIO_WHATSAPP_NUMBER: str = os.environ["TWILIO_WHATSAPP_NUMBER"]
+TELEGRAM_BOT_TOKEN: str = os.environ.get("TELEGRAM_BOT_TOKEN", "dummy")
+GREEN_API_INSTANCE_ID: str = os.environ["GREEN_API_INSTANCE_ID"]
+GREEN_API_TOKEN: str = os.environ["GREEN_API_TOKEN"]
 OPENAI_API_KEY: str = os.environ["OPENAI_API_KEY"]
